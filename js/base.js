ec7ca8a5605967bb976d4e4df0dea9a43a696fd6
流星的泪痕 <61955365@qq.com>
2014-06-23 10:32:53 +0800

    repair

--- js/base.js
+++ js/base.js
@@ -2,17 +2,18 @@
 
 
 /**
-* @name     :
-* @author   :si
-* @dependent:全局变量的命名函数
-*  GLOBAL.namespace("A.BOOK");GLOBAL.A.BOOK.name="b";
-*/
-var GLOBAL={};
-GLOBAL.namespace=function(str){
-    var arr=str.split("."),o=GLOBAL;
-    for(i=(arr[0]=="GLOBAL")?1:0;i<ar.length;i++){
-        o[arr[i]]=o[arr[i]]||{};
-        o=o[arr[i]];
+ * @name     :
+ * @author   :si
+ * @dependent:全局变量的命名函数
+ *  GLOBAL.namespace("A.BOOK");GLOBAL.A.BOOK.name="b";
+ */
+var GLOBAL = {};
+GLOBAL.namespace = function (str) {
+    var arr = str.split("."),
+        o = GLOBAL;
+    for (i = (arr[0] == "GLOBAL") ? 1 : 0; i < ar.length; i++) {
+        o[arr[i]] = o[arr[i]] || {};
+        o = o[arr[i]];
     }
 }
 /* @end **/
@@ -27,21 +28,25 @@ function get_previousSibling(n) {
 }
 
 //返回顶部
-function toTop(){   
+function toTop() {
     //首先将#back-to-top隐藏 
     $("#back-to-top").hide();
-    
+
     //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
     $(window).scroll(function () {
         if ($(window).scrollTop() > 100) {
             $("#back-to-top").fadeIn(600);
-            $("#goReturn").stop().animate({bottom:80},800);     
-        } else {         
+            $("#goReturn").stop().animate({
+                bottom: 80
+            }, 800);
+        } else {
             $("#back-to-top").fadeOut(600);
-            $("#goReturn").stop().animate({bottom:25},800);
+            $("#goReturn").stop().animate({
+                bottom: 25
+            }, 800);
         }
     });
-   
+
     //当点击跳转链接后，回到页面顶部位置
     $("#back-to-top").click(function () {
         $("body,html").animate({
@@ -49,13 +54,13 @@ function toTop(){
         }, 400);
         return false;
     });
-    
+
 };
 
 //Google 分析代码
-(function(i, s, o, g, r, a, m) {
+(function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function() {
+    i[r] = i[r] || function () {
         (i[r].q = i[r].q || []).push(arguments)
     }, i[r].l = 1 * new Date();
     a = s.createElement(o),
@@ -111,22 +116,22 @@ function getDate() {
 
 //按钮回车键
 //btnID为元素ID,scopeID为作用域ID,作用域可为空 默认window
-function btnBindEnter(btnID,scopeID) {
+function btnBindEnter(btnID, scopeID) {
     var button = document.getElementById(btnID);
     var scope = document.getElementById(scopeID);
     var temporary;
 
-    function bindEnter(event){      
-        if(event.keyCode == 13){
+    function bindEnter(event) {
+        if (event.keyCode == 13) {
             console.log("ENTER");
             button.click();
             event.returnValue = false;
         }
     }
 
-    temporary=scope?scope:window;
-    temporary.onkeydown=function bindEnter(event){
-        if(event.keyCode == 13){
+    temporary = scope ? scope : window;
+    temporary.onkeydown = function bindEnter(event) {
+        if (event.keyCode == 13) {
             console.log("ENTER");
             button.click();
             event.returnValue = false;
@@ -135,21 +140,47 @@ function btnBindEnter(btnID,scopeID) {
 }
 
 //搜索框默认值
-function searchDefault(sID){
-    var searchID=document.getElementById(sID);
+function searchDefault(sID) {
+    var searchID = document.getElementById(sID);
     //获得焦点
-    searchID.onfocus=function(){
-        if (searchID.value=="SEARCH") {
-            searchID.value="";
-        }      
+    searchID.onfocus = function () {
+        if (searchID.value == "SEARCH") {
+            searchID.value = "";
+        }
     };
     //失去焦点
-    searchID.onblur=function(){
-        if (searchID.value=="") {
-            searchID.value="SEARCH";
+    searchID.onblur = function () {
+        if (searchID.value == "") {
+            searchID.value = "SEARCH";
         }
-    }; 
+    };
 }
+
+/**
+ * @name	 :loading
+ * @author	 :si
+ * @dependent:载入loading 效果
+ */
+function loading(ID) {
+    var loadID = document.getElementById(ID);
+    var i = 1;
+    loadID.style.opacity = 1;
+
+    function defer() {
+        loadID.nextSibling
+        loadID.style.opacity = loadID.style.opacity - 0.05;
+        var opa = loadID.style.opacity;
+        if (opa >= 0.1) {
+            setTimeout(defer, 20);
+            //            consoleDebug("还没结束么");
+        } else {
+            loadID.style.display = "none";
+        }
+    }
+    defer();
+}
+
+
 
 //调试函数
 function consoleDebug(x) {
@@ -158,26 +189,30 @@ function consoleDebug(x) {
 
 
 //匿名函数A
-(function(){
-    var a=1,b=2,c=3;
+(function () {
+    var a = 1,
+        b = 2,
+        c = 3;
     // var x;
-    GLOBAL.A={};
-    GLOBAL.A.a=a;
-    GLOBAL.A.b=b;
-    GLOBAL.A.c=c;
+    GLOBAL.A = {};
+    GLOBAL.A.a = a;
+    GLOBAL.A.b = b;
+    GLOBAL.A.c = c;
     // x=c+i;
 })();
 
 //匿名函数B
-(function(){
-    var d=4,e=5,f=6;
+(function () {
+    var d = 4,
+        e = 5,
+        f = 6;
     // var xx;
-    a=GLOBAL.A.a;
-    GLOBAL.B={};
-    xx=a+d;
+    a = GLOBAL.A.a;
+    GLOBAL.B = {};
+    xx = a + d;
     consoleDebug(xx);
     // alert(xx);
-    
+
 })();
 
 // 匿名函数C
--- js/common.js
+++ js/common.js
@@ -33,6 +33,8 @@ function imgTextC() {
 
         // $imgBan.get(i).style.cssText                  += ";width:" + w + "px;height:" + h + "px;";
         // $imgBan.get(i).style.cssText                  = "width:" + w + "px;height:" + h + "px;";
+        console.log('xxx');
+
     }
 
 }
